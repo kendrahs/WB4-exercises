317410c03da3a436ba8c9386ec7b25ac9bf1879a
kendrah <kentinez11@gmail.com>
2023-10-30 15:54:30 -0400

    finished label

--- ObjectScripts/label_maker.js
+++ ObjectScripts/label_maker.js
@@ -7,3 +7,20 @@ let contact1 = {
     state: "NY",
     zip: "12345"
 };
+
+let contact2 = {
+    namee: "Joe Smith",
+    address: "345 Main St",
+    city: "Notown",
+    state: "NY",
+    zip: "12345"
+};
+
+function printContact(contact) {
+    console.log(contact.namee);
+    console.log(contact.address);
+    console.log(contact.city + ", " + contact.state + " " + contact.zip);
+}
+
+printContact(contact1);
+printContact(contact2);
