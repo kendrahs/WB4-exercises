cd8bbedd40483a5067612432549062e5b86c56d7
kendrah <kentinez11@gmail.com>
2023-10-31 10:47:36 -0400

    produce code array

---
+++ demos/loopdemo1.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let foodList = ["pizza", "hamburger", "sushi", "tacos"];
+
+for (let index = 0; index < foodList.length; index++){
+    console.log(index + " " + foodList[index]);
+}
---
+++ demos/produce_code_arrays.js
@@ -0,0 +1,16 @@
+"";
+//copy from product script
+
+let allOfTheParts = [
+    "ZJQO:12345",
+    "WJZK:67890",
+    "XJYH:54368",
+    "YJXN:09876",
+    "ZJQF:24680",
+]
+
+let upperBoundPartCodes = allOfthePartcodes.length;
+
+for (let addressofPartCode = 0; addressofPartCode < upperBoundPartCodes; addressofPartCode++) {
+
+}
