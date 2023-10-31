3fdc5c91dc72e4549a0aa67412bdfe684172ba68
kendrah <kentinez11@gmail.com>
2023-10-31 11:49:02 -0400

    FInished my family

---
+++ ArrayScripts/my_family.js
@@ -0,0 +1,15 @@
+"use script";
+famNum = 6;
+let index;
+
+
+let myFamily = [
+    {name: "Jax"},
+    {name: "Mily"},
+    {name: "Rey"},
+    {name: "Lij"},
+]
+
+for (let i = 0; index < famNum; i++) {
+    console.log(myFamily)
+}
