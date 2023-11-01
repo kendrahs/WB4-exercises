385d67a109aa7fd91ffa40bed214684e741682a4
kendrah <kentinez11@gmail.com>
2023-11-01 08:29:45 -0400

    average scores

--- ArrayScripts/avg_scores.js
+++ ArrayScripts/avg_scores.js
@@ -1,4 +1,17 @@
 "use script";
 
 let myScores = [92, 98, 84, 76, 89, 99, 100];
-let yourScores = [82, 98, 94, 88, 92, 100, 100]; 
+let yourScores = [82, 98, 94, 88, 92, 100, 100];
+let i;
+
+function getAverage(scores) {
+    //loop
+    for (let i = 0; i < scores.length; i++){
+        total += scores[i];
+        console.log(myScores)
+        let scoreAverage = total/scores.length
+    }
+    
+}
+//add, div by length
+console.log("You're score average is" + scoreAverage(getAverage(yourScores)) + ".")
--- ArrayScripts/my_family.js
+++ ArrayScripts/my_family.js
@@ -3,8 +3,7 @@ famNum = 6;
 let index;
 
 
-let myFamily = ["J", "Millie", "K", "L"
-]
+let myFamily = ["J", "Millie", "K", "L"]
 
 for (let index = 0; index < famNum; index++) {
     console.log(myFamily)
