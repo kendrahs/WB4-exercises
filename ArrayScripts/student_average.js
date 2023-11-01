e9919677830ac7ccd08497d3f3226747dd100568
kendrah <kentinez11@gmail.com>
2023-11-01 09:49:45 -0400

    student average halfway and course start

---
+++ ArrayScripts/student_average.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let students = [
+    {name: "Zephaniah", scores: [100, 96, 99, 92]},
+    {name: "Pursalane", scores: [92, 89, 96, 100, 94]},
+    {name: "Siddalee", scores: [86, 72, 92]},
+    {name: "Ian", scores: [98, 84, 89, 100, 100, 76]},
+    {name: "Elisha", scores: [89, 100]},
+    {name: "Ezra", scores: [100, 99, 100, 87]}
+   ];
+   
+for (let indexOfCurrentStudent = 0; indexOfCurrentStudent < students.length ; indexOfCurrentStudent++) {
+    let currentStudent = students[indexOfCurrentStudent];
+    console.log (currentStudent.name)
+}
