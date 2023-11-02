5bff00aa987fa15429ad35a2b43dbf7d7f7668d0
kendrah <kentinez11@gmail.com>
2023-11-02 10:23:42 -0400

    second subset array

--- demos/subsetArray_demo8.js
+++ demos/subsetArray_demo8.js
@@ -1,10 +1,13 @@
+"use strict";
+//p 35; find a collection of elements that match condition
 let menu = [
     {id: 1, item: "Tacos", category: "Meal", price: 12.29},
     {id: 2, item: "Burger", category: "Meal", price: 7.29},
     {id: 3, item: "Salad", category: "Meal", price: 8.29},
     {id: 4, item: "Ice tea", category: "Drink", price: 2.19},
     {id: 5, item: "Coke", category: "Drink", price: 2.29},
-];
+   ];
+   
    function getMenuItemsInCategory(menu, category) {
     let matching = [];
     let numItems = menu.length;
@@ -12,14 +15,13 @@ let menu = [
     if (menu[i].category == category) {
     matching.push(menu[i]);
     }
-}
+    }
     return matching;
    }
-
    // show all the drinks
    let drinks = getMenuItemsInCategory(menu, "Drink");
    let numDrinks = drinks.length;
    for(let i = 0; i < numDrinks; i++) {
     console.log(drinks[i].item + 
     " $" + drinks[i].price.toFixed(2));
-    }   
+    }
